ba4bd58d151a69d4fce1a386c12d047405769a0c
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2021-01-21 13:36:26 +0100

    First Commit with back-end done

---
+++ strapi/api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2498b16c9854e5e94cc1816fd34b0319'),
+    },
+  },
+});
---
+++ strapi/api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5f68d432-432c-4306-a08b-483b265bf963'
+};
